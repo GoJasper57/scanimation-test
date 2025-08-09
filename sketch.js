79d93061d01af8e0c6740c1f667916074b96c990
GoJasper57 <artjasperjohn@gmail.com>
2025-08-09 16:47:38 -0700

    Update sketch.js
    change file name to cat walk base

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ let startTouchX = 0;
 let isTouchDragging = false;
 
 function preload() {
-  baseImage = loadImage("scanimation_base.png"); // 你的合成底图
+  baseImage = loadImage("cat walk base.png"); // 你的合成底图
 }
 
 function setup() {
